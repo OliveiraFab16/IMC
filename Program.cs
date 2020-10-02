33b2c066bab644829f072ac3b06e3cc4bd8c706f
oliveiraFAB16 <fabriciocarvalho932@gmail.com>
2020-10-02 20:58:49 -0300

    IMC

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vanessa;
+            
+            double altura;
+            double peso;
+            
+            
+            Console.WriteLine("digite sua altura");
+            altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite seu peso");
+            peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"A Area do Triangulo é: {altura = peso}");
+
+
+
+
+
+
+
+        }
+    }
+}
