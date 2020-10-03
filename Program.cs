655746fb72a7486e3f3c0f1cdf67d4fa48e50dd7
oliveiraFAB16 <fabriciocarvalho932@gmail.com>
2020-10-03 00:10:56 -0300

    imc

--- Program.cs
+++ Program.cs
@@ -6,19 +6,41 @@ namespace IMC
     {
         static void Main(string[] args)
         {
-            string vanessa;
-            
+            string nome;
+            int idade;
             double altura;
             double peso;
+            double IMC;
             
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade: ");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite seu altura: ");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite seu Peso: ");
+            peso = Convert.ToDouble(Console.ReadLine());
+
+            IMC = peso / (altura * altura);
+
+            Console.Clear();
+
+            Console.WriteLine("\t <---Dados Pessoais--->");
+            Console.WriteLine($"nome:  {nome}");
+            Console.WriteLine($"idade: {idade} anos");
+            Console.WriteLine($"Altura:  {altura}m");
+            Console.WriteLine($"peso: {Math.Round(peso, 1)}kg");
+            Console.Write("\n");
+
+            Console.WriteLine("seu resultado de IMC é = {0}", Math.Round(IMC, 1));
+
+
+
+
             
-            Console.WriteLine("digite sua altura");
-            altura = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("digite seu peso");
-            peso = double.Parse(Console.ReadLine());
-
-            Console.WriteLine($"A Area do Triangulo é: {altura = peso}");
 
 
 
